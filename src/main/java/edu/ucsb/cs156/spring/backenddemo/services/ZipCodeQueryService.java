753ac51990f8e1067b7806c4b978b8a09b115d95
BenYangSB <72826356+BenYangSB@users.noreply.github.com>
2022-10-18 19:00:06 -0700

    zipCodeQuery Controller finished

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/ZipCodeQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/ZipCodeQueryService.java
@@ -1,5 +1,7 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -7,18 +9,33 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
+@Log4j2
 public class ZipCodeQueryService {
 
     private final RestTemplate restTemplate;
+    public static final String ENDPOINT = "http://api.zippopotam.us/us/{zipcode}";
 
     public ZipCodeQueryService(RestTemplateBuilder restTemplateBuilder) {
         restTemplate = restTemplateBuilder.build();
     }
 
-    public static final String ENDPOINT = "";
-
     public String getJSON(String zipcode) throws HttpClientErrorException {
-       return "";
+
+        log.info("zipcode={}", zipcode);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, String> uriVariables = Map.of("zipcode", zipcode);
+
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+        ResponseEntity<String> re = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class,
+                uriVariables);
+        return re.getBody();
     }
 }
