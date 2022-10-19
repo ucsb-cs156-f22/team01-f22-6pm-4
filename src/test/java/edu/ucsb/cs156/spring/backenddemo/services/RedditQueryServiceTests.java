ea9dff970ca7107d90bcc510f3ca1bc6d7fd1d4d
Vyom Singh <vyomsaritsingh@ucsb.edu>
2022-10-18 18:04:05 -0700

    test case implemented + passing mvn test

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/services/RedditQueryServiceTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/services/RedditQueryServiceTests.java
@@ -25,6 +25,19 @@ public class RedditQueryServiceTests {
 
     @Test
     public void test_getJSON(){
+
+        String subreddit = "nba";
+        String expectedURL = RedditQueryService.ENDPOINT.replace("{subreddit}", subreddit);
+
+        String fakeJsonResult = "{ \"fake\" : \"result\" }";
+
+        this.mockRestServiceServer.expect(requestTo(expectedURL))
+                .andExpect(header("Accept", MediaType.APPLICATION_JSON.toString()))
+                .andExpect(header("Content-Type", MediaType.APPLICATION_JSON.toString()))
+                .andRespond(withSuccess(fakeJsonResult, MediaType.APPLICATION_JSON));
+
+        String actualResult = redditQueryService.getJSON(subreddit);
+        assertEquals(fakeJsonResult, actualResult);
         
     }
     
